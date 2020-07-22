326c6c716264dc7e845a6cdf1cc9196125cb8559
jioyoung <you@example.com>
2020-07-22 00:07:02 -0400

    add

---
+++ 227.basic-calculator-ii.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=227 lang=python3
+#
+# [227] Basic Calculator II
+#
+
+# @lc code=start
+class Solution:
+    def calculate(self, s: str) -> int:
+        # delete the space at the begining and end
+        s = s.strip()
+        if not s:
+            return 0
+        sign = '+'
+        num = 0
+        valueStack = []
+        for c in s:
+            if c.isdigit() == True:
+                num = 10*num + ord(c) - ord('0')
+            elif c == ' ':
+                continue
+            else:
+                self.pushtoStack(sign, num, valueStack)
+                num = 0
+                sign = c
+        # the last chars are always digits since strip has been used
+        # add the last number to the stack
+        self.pushtoStack(sign, num, valueStack)
+        
+        return sum(valueStack)
+            
+
+    def pushtoStack(self, c, num, stackList):
+        if c == '+':
+            stackList.append(num)
+        elif c == '-':
+            stackList.append(-num)
+        elif c == '*':
+            stackList[-1] = stackList[-1]*num
+        else:
+            stackList[-1] = int(stackList[-1]/num)
+
+
+
+        
+        
+# @lc code=end
+
---
+++ 228.summary-ranges.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=228 lang=python3
+#
+# [228] Summary Ranges
+#
+
+# @lc code=start
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+        if len(nums) == 1:
+            return [str(nums[0])]
+        count = 1
+        left = nums[0]
+        res = []
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]+1:
+                count+=1
+            else:
+                if count == 1:
+                    res.append(str(left))
+                    left = nums[i]
+                else:
+                    res.append(str(left)+"->"+str(left+count-1))
+                    count = 1
+                    left = nums[i]
+                    
+        # the last element needs to be considered
+        if count == 1:
+            res.append(str(left))
+        else:
+            res.append(str(left)+"->"+str(left+count-1))
+        return res
+
+
+        
+# @lc code=end
+
