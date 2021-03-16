58e1f69825351e8a866e08cb659225b6af2cca9d
jioyoung <you@example.com>
2021-03-16 17:20:12 -0400

    add

--- 10.regular-expression-matching.py
+++ 10.regular-expression-matching.py
@@ -7,6 +7,7 @@
 # @lc code=start
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        # 正则表达式 regular expression
         # 多一维的空间，因为求 dp_matrix[len - 1][j] 的时候需要知道 dp_matrix[len][j] 的情况，
         # 多一维的话，就可以把 对 dp_matrix[len - 1][j] 也写进循环了
         # dp_matrix[i][j]表示 text 从 i 开始到最后，pattern 从 j 开始到最后，
@@ -23,7 +24,7 @@ class Solution:
                 if i == sLen and j == pLen:
                     continue
                 # dp_matrix[i][j+2] == True and p[j+1] is *, p[j] can be ignored with 0 repeated times
-                # dp_matrix[i+1][j] == True and currentMatch, then dp_matrix[i][j] is also true
+                # dp_matrix[i+1][j+1] == True and currentMatch, then dp_matrix[i][j] is also true
                 currentMatch = (i < sLen and j < pLen) and \
                     (p[j] == s[i] or p[j] == '.')
                 if j + 1 < pLen and p[j+1] == '*':
--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -17,41 +17,40 @@
 class Solution:
     # 最长 回文 最长回文
     def longestPalindrome(self, s):
-        # maxLen = 1
-        # start = 0
-        # dp_pal = [[False for _ in range(len(s))] for _ in range(len(s))]
-        # for i in range(len(s)):
-        #     dp_pal[i][i] = True
-        #     for j in range(i):
-        #         if i == j+1:
-        #             dp_pal[j][i] = (s[i]==s[j])
-        #         else:
-        #             # i > j+1
-        #             if s[j] == s[i] and dp_pal[j+1][i-1]:
-        #                 dp_pal[j][i] = True
-        #         if dp_pal[j][i] and i-j+1>maxLen:
-        #             maxLen = i-j+1
-        #             start = j
-        # return s[start:start+maxLen]
-        sLen = len(s)
-        res = ''
-        maxL = 0
-        for i in range(sLen):
-            temp = self.outputPalindrome(i, i, s, sLen)
-            if len(temp) > maxL:
-                res = temp
-                maxL = len(temp)
-            temp = self.outputPalindrome(i, i+1, s, sLen)
-            if len(temp) > maxL:
-                res = temp
-                maxL = len(temp)
-        return res
+        maxLen = 1
+        start = 0
+        dp = [[False for j in range(len(s))] for i in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = True
+            for j in range(i):
+                if i == j+1:
+                    dp[j][i] = (s[i] == s[j])
+                else:
+                    if s[i] == s[j] and dp[j+1][i-1]:
+                        dp[j][i] = True
+                if dp[j][i] and i-j+1> maxLen:
+                    maxLen = i-j+1
+                    start = j
+        return s[start:start+maxLen]
+    #     sLen = len(s)
+    #     res = ''
+    #     maxL = 0
+    #     for i in range(sLen):
+    #         temp = self.outputPalindrome(i, i, s, sLen)
+    #         if len(temp) > maxL:
+    #             res = temp
+    #             maxL = len(temp)
+    #         temp = self.outputPalindrome(i, i+1, s, sLen)
+    #         if len(temp) > maxL:
+    #             res = temp
+    #             maxL = len(temp)
+    #     return res
 
-    def outputPalindrome(self, left, right, s, sLen):
-        while left >= 0 and right < sLen and s[left] == s[right]:
-            left-=1
-            right+=1
-        return s[left+1:right]
+    # def outputPalindrome(self, left, right, s, sLen):
+    #     while left >= 0 and right < sLen and s[left] == s[right]:
+    #         left-=1
+    #         right+=1
+    #     return s[left+1:right]
         
 
 
