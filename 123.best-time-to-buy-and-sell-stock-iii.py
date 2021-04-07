3bf135e9fb65883a3554496fdc8b2b42c0ffd30b
jioyoung <you@example.com>
2021-04-07 01:42:05 -0400

    add

--- 123.best-time-to-buy-and-sell-stock-iii.py
+++ 123.best-time-to-buy-and-sell-stock-iii.py
@@ -63,8 +63,25 @@ class Solution(object):
         # preprofit[i] 又买有卖到i最大利润
         # postprofit[i] 从i开始 买卖 最大利润
         '''
-        dp[k][j] = max(dp[k][j-1], prices[j] + localMax)
+        # 股票 最多k次 有限次数
+        # 用 dp[k][j] 表示前j天最多交易k次的最高收益，
+        # 首先第 j 天可以什么都不操作，今天的最高收益就等于昨天的最高收益
+        # dp[k][j] = dp[k][j-1]
+        # 此外，为了获得更大收益我们第 j 天也可以选择卖出，既然选择卖出，
+        # 那么在0到 j-1 天就要选择一天买入。
+        # 多选择了一次买入，那在买入之前已经进行了 k-1 次买卖。
+        # 在第 0 天买入，收益就是 prices[j] - prices[0] + dp[k-1][0]
+        # 在第 1 天买入，收益就是 prices[j] - prices[1] + dp[k-1][1]，
+        # 多加了前一天的最大收益
+        # 在第 2 天买入，收益就是 prices[j] - prices[2] + dp[k-1][2]，
+        # 多加了前一天的最大收益
+        # ...
+        # 在第 j-1 天买入，收益就是 prices[j] - prices[j-1] + dp[k-1][j-1]，
+        # 多加了前一天的最大收益
+        # 在第 j 天买入 prices[j] - prices[j] + dp[k-1][j]
+        # localMax is max(array of dp[k-1][a]-prices[a]) a=0,1,2....j         
         localMax = max(dp[k-1][i]-prices[i]) i=0,1,....
+        dp[k][j] = max(dp[k][j-1], prices[j] + localMax)
         '''
         k = 2
         length = len(prices)
