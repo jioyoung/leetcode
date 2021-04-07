f99ddf4cae14f0078b2483b7e3e2f1989ae76ad0
jioyoung <you@example.com>
2021-04-06 23:14:47 -0400

    add

--- 102.binary-tree-level-order-traversal.py
+++ 102.binary-tree-level-order-traversal.py
@@ -53,7 +53,7 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        # 分层 树 遍历
+        分层 树 遍历
         if not root:
             return []
         nodeQueue = [root]
@@ -70,6 +70,18 @@ class Solution(object):
                     nodeQueue.append(node.right)
             res.append(subList)
         return res
+
+    #     res = []
+    #     self.dfs(root, 0, res)
+    #     return res
+
+    # def dfs(self, root, level, res):
+    #     if root:
+    #         if len(res) < level + 1:
+    #             res.append([])
+    #         res[level].append(root.val)
+    #         self.dfs(root.left, level+1, res)
+    #         self.dfs(root.right, level+1, res)
 
 # @lc code=end
 
--- 103.binary-tree-zigzag-level-order-traversal.py
+++ 103.binary-tree-zigzag-level-order-traversal.py
@@ -78,5 +78,21 @@ class Solution(object):
             
         return res
 
+    #     res = []
+    #     self.dfs(root, 0, res)
+    #     return res
+
+    # def dfs(self, root, level, res):
+    #     if root:
+    #         if len(res) < level + 1:
+    #             res.append([])
+    #         if level%2 == 0:
+    #             res[level].append(root.val)
+    #         else:
+    #             res[level].insert(0, root.val)
+    #         self.dfs(root.left, level+1, res)
+    #         self.dfs(root.right, level+1, res)
+
+
 # @lc code=end
 
