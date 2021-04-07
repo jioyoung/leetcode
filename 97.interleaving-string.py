2815a247522e5ce6e63b5bcddba52e1e9638279e
jioyoung <you@example.com>
2021-04-06 22:18:03 -0400

    add

--- 97.interleaving-string.py
+++ 97.interleaving-string.py
@@ -8,14 +8,14 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         '''
-        我们定义一个 boolean 二维数组 dp [ i ] [ j ] 
-        来表示 s1[ 0, i ) 和 s2 [ 0, j ） 
-        组合后能否构成 s3 [ 0, i + j )，
+        我们定义一个 boolean 二维数组 dp[i][j] 
+        来表示 s1[0,i) 和 s2 [0,j） 
+        组合后能否构成 s3[0, i + j)，
         注意不包括右边界，主要是为了考虑开始的时候如果只取 s1，
         那么 s2 就是空串，这样的话 dp[i][0] 就能表示 s2 取空串。
-        状态转换方程也很好写了，如果要求 dp [i] [j] 。
-        如果 dp [i-1][j] == true，并且 s1[i-1] == s3[i+j-1]，dp[i][j]=true 。
-        如果 dp [i][j-1]==true，并且 s2[j-1]==s3[i+j-1] dp[i][j]=true 。
+        状态转换方程也很好写了，如果要求 dp[i][j]
+        如果 dp[i-1][j] == true，并且 s1[i-1] == s3[i+j-1]，dp[i][j]=true 
+        如果 dp[i][j-1]==true，并且 s2[j-1]==s3[i+j-1] dp[i][j]=true 
         否则的话，就更新为 dp[i][j] = false。
         '''        
         if len(s1) + len(s2) != len(s3):
