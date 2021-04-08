1c9caaee75f2e39ff781bc38ac0f5c98e3ac21ea
jioyoung <you@example.com>
2021-04-07 21:45:32 -0400

    add

--- 199.binary-tree-right-side-view.py
+++ 199.binary-tree-right-side-view.py
@@ -14,21 +14,33 @@
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
         # 最右 树
-        if not root:
-            return []
+        # if not root:
+        #     return []
+        # res = []
+        # nodeQueue = [root]
+        # while nodeQueue:
+        #     level_len = len(nodeQueue)
+        #     for i in range(level_len):
+        #         node = nodeQueue.pop(0)
+        #         if i == level_len-1:
+        #             res.append(node.val)
+        #         if node.left:
+        #             nodeQueue.append(node.left)
+        #         if node.right:
+        #             nodeQueue.append(node.right)
+        # return res
         res = []
-        nodeQueue = [root]
-        while nodeQueue:
-            level_len = len(nodeQueue)
-            for i in range(level_len):
-                node = nodeQueue.pop(0)
-                if i == level_len-1:
-                    res.append(node.val)
-                if node.left:
-                    nodeQueue.append(node.left)
-                if node.right:
-                    nodeQueue.append(node.right)
+        self.getRighest(root, 0, res)
         return res
+
+    def getRighest(self, root, level, res):
+        if not root:
+            return
+        if level == len(res):
+            res.append(root.val)
+        self.getRighest(root.right, level+1, res)
+        self.getRighest(root.left, level+1, res)
+        return
         
         
 # @lc code=end
