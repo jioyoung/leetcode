a1188dce7d7db3f5ff22b14356dee830fdbd61a0
jioyoung <you@example.com>
2022-01-15 23:50:15 -0800

    add

--- 547.number-of-provinces.py
+++ 547.number-of-provinces.py
@@ -7,6 +7,24 @@
 # @lc code=start
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        
+        res = 0
+        visit = set()
+        length = len(isConnected)
+        for i in range(length):
+            if i not in visit:
+                res+=1
+                self.dfs_circle(isConnected, length, i, visit)
+        return res        
+    
+    def dfs_circle(self, M, length, i, visit):
+        visit.add(i)
+        for j in range(length):
+            if M[i][j] == 0 or j in visit:
+                continue
+            else:
+                self.dfs_circle(M, length, j, visit)
+        return
+
+
 # @lc code=end
 
