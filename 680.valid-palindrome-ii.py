2d7c67bad2931f566abf0f3362e7955db95fb072
Yang <joyzhyang@gmail.com>
2023-11-26 12:17:48 -0800

    add

--- 680.valid-palindrome-ii.py
+++ 680.valid-palindrome-ii.py
@@ -7,6 +7,25 @@
 # @lc code=start
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] == s[right]:
+                left+=1
+                right-=1
+            else:
+                return self.verifyPalindrome(s[left+1:right+1]) \
+                    or self.verifyPalindrome(s[left:right])
+        return True
+
+    def verifyPalindrome(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] == s[right]:
+                left+=1
+                right-=1
+            else:
+                return False
+        return True
         
 # @lc code=end
 
